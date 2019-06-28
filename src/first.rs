479ffa47015f5c1dde3f228f37ff0ceb97683961
Ozaki Yoichi <zak@nli.ics.keio.ac.jp>
2019-06-28 12:35:59 +0900

    imlemented first version of bitonic_sorter module

--- src/first.rs
+++ src/first.rs
@@ -1,11 +1,46 @@
 pub fn sort(x: &mut [u32], up: bool) {
-    unimplemented!()
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        sort(&mut x[..mid_point], true);
+        sort(&mut x[mid_point..], false);
+        sub_sort(x, up)
+    }
 }
 
 fn sub_sort(x: &mut [u32], up: bool) {
-    unimplemented!()
+    if x.len() > 1 {
+        compare_and_swap(x, up);
+        let mid_point = x.len() / 2;
+        sub_sort(&mut x[..mid_point], up);
+        sub_sort(&mut x[mid_point..], up);
+    }
 }
 
 fn compare_and_swap(x: &mut [u32], up: bool) {
-    unimplemented!()
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if (x[i] > x[mid_point + i]) == up {
+            x.swap(i, mid_point + i);
+        }
+    }
+}
+
+#[cfg(test)] // 以下のモジュールは `cargo test` を実行した時のみコンパイルされることを表すアトリビュート
+mod tests { // `first` モジュールの子供として `tests` モジュールを定義
+    use super::sort; // 親モジュール `first` の `sort` 関数を用いることを宣言している
+
+    #[test] // `cargo test` を実行した時に実行されることを表すアトリビュート
+    fn sort_u32_ascending() {
+        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        sort(&mut x, true);
+        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
+    }
+
+    #[test]
+    fn sort_u32_descending() {
+        let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        sort(&mut x, false);
+        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    }
+
 }
