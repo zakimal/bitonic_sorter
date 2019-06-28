061165832fc8b9bfc681981b25f69cf0e5b4cd88
Ozaki Yoichi <zak@nli.ics.keio.ac.jp>
2019-06-28 12:22:22 +0900

    initial commit

---
+++ src/first.rs
@@ -0,0 +1,11 @@
+pub fn sort(x: &mut [u32], up: bool) {
+    unimplemented!()
+}
+
+fn sub_sort(x: &mut [u32], up: bool) {
+    unimplemented!()
+}
+
+fn compare_and_swap(x: &mut [u32], up: bool) {
+    unimplemented!()
+}
